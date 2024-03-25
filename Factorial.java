4377648dcbc7d83dd9081b808fd42260e2a5f47e
AdrianK47 <kangadrian76@gmail.com>
2024-03-25 14:49:14 -0400

    rheheheee

---
+++ Factorial.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Factorial {
+    public static void main(String args[]){
+        int number;
+        int factorial = 1;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        number = input.nextInt();
+
+        for (int i = 1; i < number; i++){
+            factorial *= i;
+        }
+        System.out.println("This is the factorial from 1 to " + number + " : " + factorial);
+    }
+}
---
+++ OddSum.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class OddSum {
+    public static void main(String args[]){
+        int number;
+        int sum = 0;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        number = input.nextInt();
+
+        for (int i = 1; i <= number; i++){
+            if (i % 2 == 1){
+                sum += i;
+                System.out.println(sum);
+            }
+        }
+        System.out.println("The sum of the odd numbers within 1 to " + number + " is: " + sum);
+    }
+}
